276fa25d1d9f59559cbda1770b3a10b295ebdce0
edgarmuyomba <edgarmatthew247@gmail.com>
2023-12-28 19:00:36 +0300

    highlights animations

--- src/components/App/Highlights.jsx
+++ src/components/App/Highlights.jsx
@@ -3,13 +3,16 @@ import styles from './highlights.module.css';
 export default function Highlights() {
     return (
         <section className={styles.highlights}>
-            <div className="highlight1">
+            <div className={styles.highlight1}>
+                <p className="desc">Air Rifles Guns</p>
                 <img src="src/assets/highlight1.png" alt="" />
             </div>
             <div className={styles.highlight2}>
+                <p className="desc">Hand Grenades</p>
                 <img src="src/assets/highlight2.png" alt="" />
             </div>
             <div className={styles.highlight3}>
+                <p className="desc">Scoped Rifles</p>
                 <img src="src/assets/highlight3.png" alt="" />
             </div>
         </section>
