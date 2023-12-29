8ec2d20c25d0f92e84b095e30efb47fe16d1ff69
edgarmuyomba <edgarmatthew247@gmail.com>
2023-12-29 14:52:17 +0300

    count logic

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -8,6 +8,18 @@ export default function Product({ product, images }) {
     const [selected, setSelected] = useState(false);
     const [count, setCount] = useState(1);
 
+    const handleCount = (operation) => {
+        if (operation === "add") {
+            if (count < 99) setCount((count) => count + 1);
+        } else if (operation === "sub") {
+            if (count > 1) {
+                setCount((count) => count - 1);
+            } else {
+                setSelected(false);
+            }
+        }
+    }
+
     return (
         <Link className={styles.product}>
             <div className={styles.product}>
@@ -22,11 +34,11 @@ export default function Product({ product, images }) {
                         selected
                             ? ( // cart logic also here
                                 <div className={styles.count}>
-                                    <button className={styles.sub} onClick={() => setCount((count) => count - 1)}>
+                                    <button className={styles.sub} onClick={() => handleCount("sub")}>
                                         -
                                     </button>
                                     {count}
-                                    <button className={styles.plus} onClick={() => setCount((count) => count + 1)}>
+                                    <button className={styles.plus} onClick={() => handleCount("add")}>
                                         +
                                     </button>
                                 </div>
